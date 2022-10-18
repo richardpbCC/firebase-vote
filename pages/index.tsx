5b869687283d17e2ae5e4b95249fa23c58407155
richardpb <rpbeavis@gmail.com>
2022-10-19 00:23:18 +0900

    feat: integrate firestore

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,35 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import firebase from "../firebase/config";
-import {useAuthState} from "react-firebase-hooks/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 export default function Home() {
+  const [user, loading, error] = useAuthState(firebase.auth());
+  console.log("Loading:", loading, "|", "Current user:", user);
+
+  const [votes, votesLoading, votesError] = useCollection(
+    firebase.firestore().collection("votes"),
+    {},
+  );
+
+  if (!votesLoading && votes) {
+    votes.docs.map((doc) => console.log(doc.data()));
+  }
+
   return (
-    <div>Hello</div>
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        width: "100vw",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      <button style={{ fontSize: 32, marginRight: 8 }}>🐱</button>
+      <button style={{ fontSize: 32, marginRight: 8 }}>🐶</button>
+      <button style={{ fontSize: 32, marginRight: 8 }}>🐱🐶</button>
+    </div>
   )
 }
